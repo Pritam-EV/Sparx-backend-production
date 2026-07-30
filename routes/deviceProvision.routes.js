6f7cb49fc79e7e3262df8b6e89dc2d5e04fe72e2
Pritam-EV <pritam.ev@gmail.com>
2026-07-30 23:50:07 +0530

    productionstages

--- routes/deviceProvision.routes.js
+++ routes/deviceProvision.routes.js
@@ -11,7 +11,7 @@ const {
 } = require('../controllers/deviceProvision.controller');
 
 // All routes require admin
-router.use(verifyToken, requireRole('admin'));
+router.use(verifyToken, authorizeRoles('admin'),);
 
 router.post('/group-a',          createGroupA);
 router.get('/group-a',           getAllGroupA);
